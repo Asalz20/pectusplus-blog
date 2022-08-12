49c68267084e8dfc6641d92f61cb16d55d5cd15f
Alex Salazar <helloworld3197@gmail.com>
2022-08-11 23:30:44 -0400

    Created desktop and mobile header

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-const IndexPage = () => (
-  <>
-    <h1>Welcome to PectusPlus.blog</h1>
-    <h2>
-      - by{' '}
-      <a href="https://asalazar.dev/" target="_blank" rel="noreferrer">
-        Alex Salazar
-      </a>
-    </h2>
-  </>
-);
+const IndexPage = () => null;
 
 export default IndexPage;
