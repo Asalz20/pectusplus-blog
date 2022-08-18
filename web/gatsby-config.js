fddb5c0872b135275ecd904832574a7106888353
Alex Salazar <helloworld3197@gmail.com>
2022-08-18 13:36:32 -0400

    Added React Helmet/SEO

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,8 +7,10 @@ module.exports = {
     description: `PectusPlus Blog is a site for all things Pectus Excavatum related. Blog posts will dive deep on the review of published medical journals, stretching/mobility work, weightlifting routines, and breathing techniques. `,
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
   ],
 };
